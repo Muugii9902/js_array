193e4d58bd1be436c7bf28a2548082216dc4a494
Muugii9902 <muugii9902@gmail.com>
2024-06-18 13:08:16 +0800

    js_array

--- function.js
+++ function.js
@@ -59,46 +59,53 @@ function ihbga(x, y) {
 let dun = ihbga(20, 50);
 console.log("ihtoo  ", dun);
 // bodlogo 2
+let arr1 = [1, 2, 3, 4, 5];
 
-let arrayofnumbers = [43, 56, 23, 89, 88, 90];
-let num = 43;
-let num1 = -1;
-function number1(arrayofnumbers) {
-  for (i = 0; i < arrayofnumbers.length; i++) {
-    if (arrayofnumbers[i] !== 43) {
-      return num;
-    } else {
-      return num1;
+function findIndex(searchArr, findtoo) {
+  console.log("search arr1:", searchArr);
+  console.log("find number:", findtoo);
+  for (let i = 0; i <= searchArr.length - 1; i++) {
+    if (searchArr[i] === findtoo) {
+      return i;
     }
   }
+  return -1;
 }
-number1(arrayofnumbers);
-console.log("too", num);
+let idx = findIndex(arr1, 6);
+console.log("index:", idx);
+
+idx = findIndex([123, 45, 67, 78, 98], 100);
+console.log("index:", idx);
+
+// bodlogo 3 Өгөгдсөн тэмдэгт мөрөөс -аас өгөгдсөн тэмдэгт байгаа эсэхийг олох функц бич. Тухайн тэмдэгт байвал индексийг буцаана. Байхгүй бол -1 буцаана.
+
 // bodlogo 4
 let arrayN = [1, 2, 3, 4, 5];
 function Average(arrayN) {
-  let sum4 = 0;
+  let sum = 0;
   for (let i = 0; i < arrayN.length; i++) {
-    sum4 = sum4 + arrayN.length[i];
+    sum = sum + arrayN[i];
   }
-  return sum4 / arrayN.length;
+
+  return sum / arrayN.length;
 }
-let averagesum4 = Average(arrayN);
-console.log("dundaj", averagesum4);
+let averagesum = Average(arrayN);
+console.log("dundaj", averagesum);
 // bodlogo 5
 let arr = [1, 2, 3, 4, 5];
 function sumArray(arr) {
   let dum = 0;
   for (let i = 0; i < arr.length; i++) {
-    dum += arr.length[i];
+    dum += arr[i];
   }
   return dum;
 }
-let totaldum = sumArray(arr);
-console.log("niilber", totaldum);
+let totalsum = sumArray(arr);
+console.log("niilber", totalsum);
 // bodlogo 6
 function isPrime(num) {
   if (num <= 1) return false;
+  if (num === 2) return true;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
       return false;
@@ -106,9 +113,90 @@ function isPrime(num) {
     return true;
   }
 }
-console.log(isPrime(7));
+console.log("prime", isPrime(7));
+// bodlogo 7 Palindrome too eseh jishee ni: input: 121 output: true, input: -121 output: false, input: 12  output: false
+let num = 1221;
+console.log(num, "nen too palindrom uu", palindrom(num));
+function palindrom(num) {
+  let firstNum = num;
+  let dig = 0;
+  while (num > 0) {
+    dig = dig * 10 + (num % 10);
+    num = (num - (num % 10)) / 10;
+  }
+  if (dig === firstNum) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 // bodlogo 8
 let str = "HELLO";
 console.log(str);
 let = output = str.split("").reverse().join("");
 console.log(output);
+
+// max number
+// function max(z,y){
+//   if (z>y){
+//     return z;
+//   }else if(z>y){
+//     return y;
+//   }
+// }
+// let mm = Math.max(12, 11);
+// console.log("max", mm);
+// let v = Math.floor(9.56);
+// console.log("number", v);
+// let v1 = Math.ceil(9.56);
+// console.log("number", v1);
+// v1 = Math;
+// let random = Math.ceil(Math.random() * 50);
+// console.log("random too", random);
+let students = [
+  {
+    name: "Сэд-Эрдэнэ",
+    age: 19,
+    gender: "male",
+    points: 70,
+  },
+  {
+    name: "Индра",
+    age: 19,
+    gender: "female",
+    points: 50,
+  },
+  {
+    name: "Хатнаа ",
+    age: 21,
+    gender: "male",
+    points: 99,
+  },
+  {
+    name: "Тэмүүлэн",
+    age: 23,
+    gender: "male",
+    points: 55,
+  },
+  {
+    name: "Намуун",
+    age: 23,
+    gender: "female",
+    points: 85,
+  },
+];
+console.log("ehnii suragchiin buh medeelel", students[0]);
+
+function er_tooloh(local_students) {
+  let sum_er = 0;
+  let sum_em = 0;
+  for (let i = 0; i < local_students.length; i++) {
+    if (local_students[i].gender === "male") {
+      sum_er = sum_er + 1;
+    }
+  }
+  return sum_er;
+}
+
+console.log("niit eregtei suragchdiin too", er_tooloh(students));
